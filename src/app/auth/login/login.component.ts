0a7262bb464958ac7bbdada49589e7261851a428
Aayushi Shukla <53366218+AayushiShukla7@users.noreply.github.com>
2024-11-20 22:08:27 -0500

    Validations added to Login form

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule,
+    CommonModule
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+
+  // Email: any;
+  // Password: any;
+
+  constructor() {}
 
 }
